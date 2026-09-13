69b857a2278a773fccafaa0f572c3c6339f4c68a
fewtarius <fewtarius@steamfork.org>
2026-09-13 05:34:49 -0400

    chore(release): prepare version 20260913.1

--- lib/CLIO.pm
+++ lib/CLIO.pm
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '20260912.3';
+our $VERSION = '20260913.1';
 
 =head1 NAME
 
@@ -15,7 +15,7 @@ CLIO - Command Line Intelligence Orchestrator
 
 =head1 VERSION
 
-Version 20260912.3
+Version 20260913.1
 
 =head1 DESCRIPTION
 
